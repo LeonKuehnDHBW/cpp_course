856e7fedc4e42a56adf9cd0f3873fd6d47eefafc
LeonKuehnDHBW <kuehnleon84@gmail.com>
2023-06-18 16:05:57 +0200

    main.cpp: created object of Professor class and MailHandler class, test function writeMail of MailHandler

--- 2_OOP/3_Person/main.cpp
+++ 2_OOP/3_Person/main.cpp
@@ -3,9 +3,13 @@
 //
 
 #include <iostream>
-
+#include "Professor.hpp"
+#include "MailHandler.hpp"
 
 int main()
 {
+    Professor prof1("Herr Drüppel", "drüppel@gmail.com", "Suttgart", "DE95 82972923729");
+    MailHandler mail;
 
+    mail.writeMail(prof1, "Ich habe eine Frage zum Aufstellen einer Differntialgleichung");
 }
